db85e587a36402ddbe11389522c20a3cb63c49a7
AmitVermaGenp <Amit.Verma2e84c5@Genpact.com>
2017-12-28 08:17:45 +0000

    adding flder

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(13, 6);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(218, 39);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 133);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(45, 40);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(73, 133);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(45, 40);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(133, 133);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(45, 40);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "+";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(189, 133);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(45, 40);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "=";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(242, 314);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        string operand1 = string.Empty;
+        string operand2 = string.Empty;
+        string result;
+        char operation;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Click += new EventHandler(btn_Click);
+            button2.Click += new EventHandler(btn_Click);
+            
+           
+           
+           
+        }
+        void btn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Button btn = sender as Button;
+
+                switch (btn.Name)
+                {
+                    case "button1":
+                        richTextBox1.Text += "1";
+                        break;
+                    case "button2":
+                        richTextBox1.Text += "2";
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Sorry for the inconvenience, Unexpected error occured. Details: " +
+                    ex.Message);
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            operand1 = richTextBox1.Text;
+            operation = '+';
+            richTextBox1.Text = string.Empty;
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            operand2 = richTextBox1.Text;
+
+            double opr1, opr2;
+            double.TryParse(operand1, out opr1);
+            double.TryParse(operand2, out opr2);
+
+            switch (operation)
+            {
+                case '+':
+                    result = (opr1 + opr2).ToString();
+                    break;
+
+                case '-':
+                    result = (opr1 - opr2).ToString();
+                    break;
+
+                case '*':
+                    result = (opr1 * opr2).ToString();
+                    break;
+
+                case '/':
+                    if (opr2 != 0)
+                    {
+                        result = (opr1 / opr2).ToString();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Can't divide by zero");
+                    }
+                    break;
+            }
+
+            richTextBox1.Text = result.ToString();
+        }
+    }
+}
